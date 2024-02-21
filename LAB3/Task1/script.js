e55a4792ec3e71a5dfaf5ab24bc078d4b8c9d61f
1245656 <dmmmaster197@gmail.com>
2024-02-21 23:50:38 +0600

    lab3

---
+++ LAB3/Task1/script.js
@@ -0,0 +1,13 @@
+"use strict";
+alert( 'В ваш компютер проник вирус' );
+
+alert (1
+    + 3
+    + 2)
+
+alert("Hello")
+[1,2].forEach(alert);
+
+/* Comment comment comment */
+// Comment comment like in java i guess
+
